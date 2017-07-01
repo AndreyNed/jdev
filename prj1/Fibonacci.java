d0268319424adfee53df0eb3a942fee34e4437d1
a_nedaseikin <andrei.nedaseikin@gmail.com>
2017-07-01 17:28:21 +0300

    fibonacci`s numbers (even)

--- prj1/Fibonacci.java
+++ prj1/Fibonacci.java
@@ -9,12 +9,18 @@ public class Fibonacci {
 		final int MIN = 1;
 		long lNumber = 0;
 		long hNumber = 1;
+		char even = ' ';
 		System.out.println("Fibonacci`s numbers (from " + MIN + " to " + (MAX - 1) + "):");
 		//System.out.println("1: " + MIN);
 		for ( int i = 1; i < 50; i++ ) {
 			hNumber+= lNumber;
 			lNumber = hNumber - lNumber;
-			System.out.println( i + ": " + lNumber );
+			if ( lNumber % 2 == 0 ) {
+				even = '*';
+			} else {
+				even = ' ';
+			}
+			System.out.println( i + ": " + lNumber + even);
 		}
 	}
 
