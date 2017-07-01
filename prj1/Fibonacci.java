4b65c348d8acc3b9e925c3204195bb31fbee8588
a_nedaseikin <andrei.nedaseikin@gmail.com>
2017-07-01 17:07:39 +0300

    fibonacci numbers

---
+++ prj1/Fibonacci.java
@@ -0,0 +1,21 @@
+package prj1;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Output Fibonacci`s numbers < 50*/
+		final int MAX = 50;
+		final int MIN = 1;
+		long lNumber = 0;
+		long hNumber = 1;
+		System.out.println("Fibonacci`s numbers (from " + MIN + " to " + (MAX - 1) + "):");
+		//System.out.println("1: " + MIN);
+		for ( int i = 1; i < 50; i++ ) {
+			hNumber+= lNumber;
+			lNumber = hNumber - lNumber;
+			System.out.println( i + ": " + lNumber );
+		}
+	}
+
+}
